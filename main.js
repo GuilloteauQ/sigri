462d0a6f07ce68d4bd3c77c6a0af8905dbccc418
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2022-02-12 21:34:14 +0100

    some improvements

--- main.js
+++ main.js
@@ -2,31 +2,24 @@ import init, {js_start} from './pkg/sigrilib.js'
 
     await init()
 
-var filename = '/tmp/output_sigri.csv'
-
-export function readFile(input) {
-    let file = input.files[0];
-
-    let reader = new FileReader();
-
-    reader.readAsText(file);
-
-    reader.onload = function() {
-        let csv_content = js_start(reader.result)
-        var blob = new Blob([csv_content], {type: 'text/plain;charset=utf-8'});
-        var a = document.getElementById('downloadCSV');
-        a.href = URL.createObjectURL(blob);
-    };
-}
-
-export function download(text, name, type) {
-    var a = document.getElementById('a');
-    var file = new Blob([text], {type: type});
-    a.href = URL.createObjectURL(file);
-    a.download = name;
-}
-
 var configFile = document.getElementById('configFile')
-configFile.onchange = evt => {
-    readFile(configFile)
+configFile.onchange =
+    evt => {
+        let file = configFile.files[0];
+        let reader = new FileReader();
+        reader.readAsText(file);
+
+        reader.onload = function() {
+            document.getElementById('configArea').value = reader.result;
+        };
+    }
+
+var buttonRun = document.getElementById('buttonRun')
+buttonRun.onclick = evt => {
+    let config = document.getElementById('configArea').value;
+    let csv_content = js_start(config)
+    var blob = new Blob([csv_content], {type: 'text/plain;charset=utf-8'});
+    var a = document.getElementById('downloadCSV');
+    a.href = URL.createObjectURL(blob);
 }
+
